c36a5554784aeb0c140119fdf6582dfb16f191f0
Konstantin Makarov <makarov.k@mail.ru>
2024-12-24 10:36:33 +0300

    Add files via upload

--- game_shop/task1/models.py
+++ game_shop/task1/models.py
@@ -8,6 +8,9 @@ class Buyer(models.Model):
     balance = models.DecimalField(max_digits=6, decimal_places=2)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Game(models.Model):
     title = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=6, decimal_places=2)
@@ -15,3 +18,6 @@ class Game(models.Model):
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer,related_name='games')
+
+    def __str__(self):
+        return self.title
